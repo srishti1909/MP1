52f5fd973ec790b75a85de8d39d600fe0b7ee3b5
srishti1909 <srishtisharma.ss321@gmail.com>
2023-09-04 20:44:37 -0500

    Update example.py

--- example.py
+++ example.py
@@ -4,7 +4,15 @@ def tokens_lowercase(doc):
     #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"), 
     #lowercases, removes words with less than 2 and more than 5  characters
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
-    '''Place your code here'''
+    #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"),
+    tokenizer = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+    #lowercases, removes words with less than 2 and more than 5  characters
+    lowercase_filter = metapy.analyzers.LowercaseFilter(tokenizer)
+    length_filter = metapy.analyzers.LengthFilter(lowercase_filter, min=2, max=5)
+    #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
+    stemmer = metapy.analyzers.Porter2Filter(length_filter)
+    trigram = metapy.analyzers.NGramWordAnalyzer(3, stemmer)
+    trigrams = trigram.analyze(doc)
     
     #leave the rest of the code as is
     tok.set_content(doc.content())
