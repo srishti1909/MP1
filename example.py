8cd66dd122ed0bb126b27f4380106d1663b41a6b
srishti1909 <srishtisharma.ss321@gmail.com>
2023-09-05 11:54:12 -0500

    updated
    example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import metapy
 
 def tokens_lowercase(doc):
     #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"), 
-    #lowercases, removes words with less than 2 and more than 5  characters
+    #lowercases, removes words with less than 2 and more than 5  characters. 
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
     #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"),
     tokenizer = metapy.analyzers.ICUTokenizer(suppress_tags=True)
